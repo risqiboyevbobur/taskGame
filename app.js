846963a5c88731d0e2552cbd33283f0a6bb27750
risqiboyevbobur <brisqiboyev@gmail.com>
2023-05-04 21:45:03 +0500

    game

---
+++ app.js
@@ -0,0 +1,14 @@
+
+const App = {
+   $:{
+    menu:document.querySelector(".menu"),
+    menuItems:document.querySelector(".items")
+   } ,
+   init(){
+    App.$.menu.addEventListener("click", event => {
+        App.$.menuItems.classList.toggle("hidden")
+        })
+   }
+}
+
+window.addEventListener("load",App.init)
